becd25b4bacae1807950218796b61d66f113072a
Sondre Eftedal <sondree@uia.no>
2021-01-25 18:52:54 +0100

    Added buttons and functionality as described in assignment 1

--- Pomodoro/Pomodoro-main/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ Pomodoro/Pomodoro-main/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -14,9 +14,16 @@ class MainActivity : AppCompatActivity() {
     lateinit var timer:CountDownTimer
     lateinit var startButton:Button
     lateinit var coutdownDisplay:TextView
+    lateinit var ninetymin:Button
+    lateinit var thirtymin:Button
+    lateinit var sixtymin:Button
+    lateinit var hundredandtwentymin:Button
 
-    val timeToCountDownInMs = 5000L
+    //val timeToCountDownInMs = 5000L
     val timeTicks = 1000L
+    var timeToCountDownInMs = 5000L
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +34,31 @@ class MainActivity : AppCompatActivity() {
            startCountDown(it)
        }
        coutdownDisplay = findViewById<TextView>(R.id.countDownView)
+        val ninety = findViewById<Button>(R.id.nintymin)
+        ninety.setOnClickListener(){
+            timeToCountDownInMs = 90*60*1000
+        }
+
+        val thirty = findViewById<Button>(R.id.thirtymin)
+        thirty.setOnClickListener(){
+            timeToCountDownInMs = 30*60*1000
+        }
+
+        val sixty = findViewById<Button>(R.id.sixtymin)
+        sixty.setOnClickListener(){
+            timeToCountDownInMs = 60*60*1000
+        }
+
+        val hundredandtwenty = findViewById<Button>(R.id.hundredandtwentymin)
+        hundredandtwenty.setOnClickListener(){
+            timeToCountDownInMs = 120*60*1000
+        }
 
     }
 
-    fun startCountDown(v: View){
 
+    fun startCountDown(v: View){
+        if(coutdownDisplay.text == "00:00:00"){
         timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
             override fun onFinish() {
                 Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
@@ -42,7 +69,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        timer.start()
+        timer.start()}
     }
 
     fun updateCountDownDisplay(timeInMs:Long){
